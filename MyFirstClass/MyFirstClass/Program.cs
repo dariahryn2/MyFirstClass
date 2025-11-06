ceef9a4852066c4589586c69a95e6c226c318fe5
dariahryn <Daria.Hryn@student.gildeopleidingen.nl>
2025-11-06 13:16:24 +0100

    ..

---
+++ MyFirstClass/MyFirstClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyFirstClass;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stoel rodeStoel = new Stoel("rood", 60);
+        Console.WriteLine(rodeStoel.echoKleur());
+        Console.WriteLine("De stoel heeft een zithoogte van: " + rodeStoel.getZithoogte());
+
+        rodeStoel.verstellen(10);
+        Console.WriteLine("Na verstellen is de zithoogte: " + rodeStoel.getZithoogte());
+    }
+}
+
---
+++ MyFirstClass/MyFirstClass/Stoel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstClass
+{
+    internal class Stoel
+    {
+        private string _kleur;
+        private int _zithoogte;
+
+        public Stoel(string ingegevenKleur, int ingegevenZithoogte)
+        {
+            _kleur = ingegevenKleur;
+            _zithoogte = ingegevenZithoogte;
+        }
+
+        // kleur teruggeven
+        public string echoKleur()
+        {
+            return "De kleur van de stoel is: " + _kleur;
+        }
+
+        // zithoogte teruggeven
+        public int getZithoogte()
+        {
+            return _zithoogte;
+        }
+
+        // hoogte verstellen
+        public void verstellen(int hoogte)
+        {
+            _zithoogte += hoogte;
+            Console.WriteLine("De stoel is met " + hoogte + " cm versteld.");
+        }
+    }
+}
